08984c592b10b551dfd4f0be5ec056190f4ed49c
Kaan <kaanc6286@gmail.com>
2025-08-20 16:47:24 +0300

    Entities

---
+++ MovieApi.Domain/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
